f6dbe5397efaad595e53469a368141852eeff353
Theo-Cahuzac <cahuzactheo@gmail.com>
2025-03-19 21:08:17 +0100

    Add files via upload

---
+++ version_1/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const productController = require('./productController');
+const { swaggerUi, specs } = require("swagger");
+
+const app = express();
+const PORT = 6942;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+console.log(`Swagger UI is running at http://localhost:${PORT}/api-docs`);
+
+app.use('/api', productController);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
