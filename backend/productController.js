9120e8ae0dc906d8acfcd35de2d052934f8d479b
Antoine <antoinebontems05@gmail.com>
2025-03-24 19:12:24 +0100

    Unification .env

--- backend/productController.js
+++ backend/productController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql2');
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
