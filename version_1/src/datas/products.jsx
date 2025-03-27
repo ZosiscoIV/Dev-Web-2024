ffa11f1e2c63bd188d0f382eceaed4d2b5e8ffe7
Antoine <antoinebontems05@gmail.com>
2025-03-27 09:46:04 +0100

    changement gitignore

--- version_1/src/datas/products.jsx
+++
@@ -1,7 +0,0 @@
-const products = [
-    { id: 1, name: "Pommes", price: 2.5, category: "fruit", image: "https://via.placeholder.com/150" },
-    { id: 2, name: "Bananes", price: 1.8, category: "fruit", image: "https://via.placeholder.com/150" },
-    { id: 3, name: "Carottes", price: 3.0, category: "légume", image: "https://via.placeholder.com/150" },
-
-    ]
-
