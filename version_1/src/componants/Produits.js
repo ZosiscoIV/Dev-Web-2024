cb5675a9c01999206fe3904b96776fbc2b82186f
Theo-Cahuzac <cahuzactheo@gmail.com>
2025-04-01 10:35:50 +0200

    Update Produits.js

--- version_1/src/componants/Produits.js
+++ version_1/src/componants/Produits.js
@@ -1,13 +1,13 @@
 import pate from "../assets/pâte.jpg";
 import "../styles/Produits.css"
 
-    const Produits = ({produit, categorie, prix, image}) => {
+    const Produits = ({produit, description, prix, image}) => {
         return (
             <div>
                 <a className="produits" href="#">
                     <img src={image} className="produitImg" alt={produit}/>
                     <h2>{produit}</h2>
-                    <p>{categorie}</p>
+                    <p>{description}</p>
                     <p><strong>Prix : {prix}€</strong></p>
                 </a>
             </div>
