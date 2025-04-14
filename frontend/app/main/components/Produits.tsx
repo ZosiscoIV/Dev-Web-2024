ef606ec627295ede4274aebbc90cb9ab8a7ba72d
Theo <theo6740@hotmail.fr>
2025-04-14 15:28:42 +0200

    Build ok + ajout de Eslint pour ignorer les tests, mais encore des choses a ajouté

--- frontend/app/main/components/Produits.tsx
+++ frontend/app/main/components/Produits.tsx
@@ -4,19 +4,26 @@ import Image from 'next/image';
 
 interface ProduitsSearchProps {
     produit: string;
-    categorie: string;
+    categorie: string[];
+    description: string;
     prix: number;
     image: string;
+    quantite: number;
+    status: string;
+    onImageError: () => void;
 }
 
-const ProduitsSearch = ({ produit, categorie, prix, image }: ProduitsSearchProps) => {
+
+const ProduitsSearch = ({ produit, categorie, prix, image, quantite, onImageError, status}: ProduitsSearchProps) => {
     return (
         <div>
             <a className="produits" href="#">
                 <h3>{produit}</h3>
                 <p>Catégorie : {categorie}</p>
                 <p>Prix : {prix} €</p>
-                <Image src={image} alt={produit} />
+                <p>Quantité : {quantite} €</p>
+                <p>Status : {status}</p> {/* Affichage du statut */}
+                <Image src={image} alt={produit} onError={onImageError} />
             </a>
         </div>
     );
--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,7 +1,7 @@
 "use client"; // Ce code est spécifiquement destiné à s'exécuter côté client
 
 import React from 'react';
-import App from './main/components/App'; // Assure-toi d'importer ton composant principal
+import App from './main/components/App';
 
 // Ce code sera exécuté sur le client, alors Next.js gère le rendu
 export default function Page() {
