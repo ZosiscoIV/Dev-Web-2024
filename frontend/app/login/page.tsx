043354f007e3aaad6b5026814d5c6d12d707219e
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-05-21 11:30:09 +0200

    Update page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import axiosClient from '../fetchWithToken'
 
 const Login = () => {
     const [email, setEmail] = useState('');
