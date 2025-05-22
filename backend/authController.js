b92b678822018e67ef5b299c9adb9060a1c0701b
TheReturnedABought <145712267+TheReturnedABought@users.noreply.github.com>
2025-05-22 12:26:07 +0200

    Update authController.js

--- backend/authController.js
+++ backend/authController.js
@@ -75,7 +75,7 @@ router.post('/register', async (req, res) => {
 
         const token = tokenGen(result.insertId, nom, prenom, email);
         res.cookie('token', token, {
-            httpOnly: true,
+            httpOnly: false,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'Strict',
             maxAge: 24 * 60 * 60 * 1000
@@ -104,7 +104,7 @@ router.post('/login', async (req, res) => {
 
         const token = tokenGen(user.id, user.nom, user.prenom, user.adresseMail);
         res.cookie('token', token, {
-            httpOnly: true,
+            httpOnly: false,
             secure: process.env.NODE_ENV === 'production',
             sameSite: 'Strict',
             maxAge: 24 * 60 * 60 * 1000
